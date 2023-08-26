b40d45adc18f41a5ebe4d654e88843cc3b9d0bd6
muhmiauwau <fabio@madless.de>
2023-08-26 10:47:50 +0200

    update locales
    fix koKR locale
    add zhTW locale

---
+++ locale/zhTW.lua
@@ -0,0 +1,21 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("AHCC", "zhTW")
+if not L then return end
+
+
+
+
+
+
+
+L["CELL_STAMINA"] = "耐力"
+L["Draconic Missive"] = "龍族書狀"
+L["Embellishments"] = "裝飾"
+L["Flasks"] = "精鍊"
+L["Food"] = "食物"
+L["Optional Reagents"] = "可選材料"
+L["Potions"] = "藥劑"
+L["Runes"] = "符文"
+L["STAMINA"] = "耐力"
+L["TABLE_HEADER_QUALITY"] = "品質"
+L["TABLE_HEADER_STAT1"] = "主屬性"
+L["TABLE_HEADER_STAT2"] = "副屬性"

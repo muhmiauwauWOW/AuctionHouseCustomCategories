83284415e204915833b619b3e9f64becd71b60bd
muhmiauwau <fabio@madless.de>
2024-09-19 11:47:29 +0200

    update data

--- data/TWW/items.lua
+++ data/TWW/items.lua
@@ -13,9 +13,9 @@ local function getCategoryId(name, ...)
 end
 
 Data.Items = {
-    -- [getCategoryId("enchants", "NECK")] = {
-    --    { ["id"] = 213777 }, -- Magnificent Jeweler's Setting
-    -- },
+    [getCategoryId("enchants", "SOCKETS")] = {
+       { ["id"] = 213777 }, -- Magnificent Jeweler's Setting
+    },
     [getCategoryId("enchants", "SHOULDER")] = {
         { ["id"] = { 200614, 200615, 200616 } }, -- Illusory Adornment: Fire
         { ["id"] = { 201312, 201311, 201310 } }, -- Illusory Adornment: Frost
@@ -90,7 +90,7 @@ Data.Items = {
         { ["id"] = { 223773, 223774, 223775 } }, -- Enchant Weapon - Authority of Air
         { ["id"] = { 223776, 223777, 223778 } }, -- Enchant Weapon - Authority of Fiery Resolve
         { ["id"] = { 223779, 223780, 223781 } }, -- Enchant Weapon - Authority of Radiant Power
-        -- wait for Season 1 { ["id"] = { 223782, 223783, 223784 } }, -- Enchant Weapon - Authority of the Depths
+        { ["id"] = { 223782, 223783, 223784 } }, -- Enchant Weapon - Authority of the Depths
     },
     [getCategoryId("enchants", "TOOL")] = {
         { ["id"] = { 223693, 223694, 223695 } }, -- Enchant Tool - Algari Deftness
@@ -201,12 +201,12 @@ Data.Items = {
         { ["id"] = { 221938, 221939, 221940 } }, -- Concealed Chaos Module
         { ["id"] = { 221941, 221942, 221943 } }, -- Energy Redistribution Beacon
         { ["id"] = { 219495, 219496, 219497 } }, -- Blessed Weapon Grip
-        -- wait for Season 1 { ["id"] = { 222452, 222453, 222454 } }, -- Echoing Impact Seal
-        -- wait for Season 1 { ["id"] = { 222455, 222456, 222457 } }, -- Arathor Armor Patch
+        { ["id"] = { 222452, 222453, 222454 } }, -- Echoing Impact Seal
+        { ["id"] = { 222455, 222456, 222457 } }, -- Arathor Armor Patch
         { ["id"] = { 219504, 219505, 219506 } }, -- Writhing Armor Banding
         { ["id"] = { 222868, 222869, 222870 } }, -- Dawnthread Lining
-        -- wait for Season 1 { ["id"] = { 222871, 222872, 222873 } }, -- Duskthread Lining
-        -- wait for Season 1 { ["id"] = { 222389, 221622, 221623 } }, -- Duskthread Lining
+        { ["id"] = { 222871, 222872, 222873 } }, -- Duskthread Lining
+        { ["id"] = { 222389, 221622, 221623 } }, -- Duskthread Lining
         { ["id"] = 228921 }, -- Griftah's Heavy-Duty Embellishing Powder
     }
 }

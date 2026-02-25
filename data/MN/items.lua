3550bce3010ce800d2fc857cde83615306db094b
muhmiauwau <muhmiauwau@users.noreply.github.com>
2026-02-25 23:43:02 +0100

    Update items.lua

--- data/MN/items.lua
+++ data/MN/items.lua
@@ -16,7 +16,7 @@ Data.Items = {
     -- [getCategoryId("enchants", "SOCKETS")] = {
     --     { ["id"] = 213777 }, -- Magnificent Jeweler's Setting
     -- },
-    [getCategoryId("enchants", "SHOULDER")] = { -- done
+    [getCategoryId("enchants", "SHOULDER")] = {
         { ["id"] = { 244032, 244033 } }, -- Illusory Adornment - Blooming Light
         { ["id"] = { 244034, 244035 } }, -- Illusory Adornment - Nature's Embrace
         { ["id"] = { 244036, 244037 } }, -- Illusory Adornment - Voidtouched
@@ -29,7 +29,7 @@ Data.Items = {
     --     { ["id"] = { 223738, 223739, 223740 } }, -- Enchant Cloak - Whisper of Silken SpeedBonus   
     --     { ["id"] = { 223798, 223799, 223800 } }, -- Enchant Cloak - Chant of Burrowing Rapidity    
     -- },
-    [getCategoryId("enchants", "CHEST")] = { -- done
+    [getCategoryId("enchants", "CHEST")] = {
         { ["id"] = { 243946, 243947 } }, -- Enchant Chest - Mark of Nalorakk
         { ["id"] = { 244002, 244003 } }, -- Enchant Chest - Mark of the Magister
         { ["id"] = { 243974, 243975 } }, -- Enchant Chest - Mark of the Rootwarden
@@ -46,7 +46,7 @@ Data.Items = {
     -- [getCategoryId("enchants", "WAIST")] = {
      
     -- },
-    [getCategoryId("enchants", "LEGS")] = { -- done
+    [getCategoryId("enchants", "LEGS")] = {
         { ["id"] = { 240094, 240133 } }, -- Sunfire Silk Spellthread
         { ["id"] = { 240154, 240155 } }, -- Arcanoweave Spellthread
         { ["id"] = { 244640, 244641 } }, -- Forest Hunter's Armor Kit
@@ -55,12 +55,12 @@ Data.Items = {
         { ["id"] = { 244644, 244645 } }, -- Thalassian Scout Armor Kit
 
     },
-    [getCategoryId("enchants", "FEET")] = { -- done
+    [getCategoryId("enchants", "FEET")] = {
         { ["id"] = { 244008, 244009 } }, -- Enchant Boots - Farstrider's Hunt
         { ["id"] = { 243952, 243953 } }, -- Enchant Boots - Lynx's Dexterity
         { ["id"] = { 243982, 243983 } }, -- Enchant Boots - Shaladrassil's Roots
     },
-    [getCategoryId("enchants", "FINGER")] = { -- done
+    [getCategoryId("enchants", "FINGER")] = {
         { ["Stat1"] = 3, ["id"] = { 243954, 243955 } }, -- Enchant Ring - Amani Mastery
         { ["Stat1"] = 1, ["id"] = { 243956, 243957 } }, -- Enchant Ring - Eyes of the Eagle
         { ["Stat1"] = 1, ["id"] = { 243986, 243987 } }, -- Enchant Ring - Nature's Fury
@@ -71,7 +71,7 @@ Data.Items = {
         { ["Stat1"] = 4, ["id"] = { 244012, 244013 } }, -- Enchant Ring - Thalassian Versatility
         { ["Stat1"] = 3, ["id"] = { 243958, 243959 } }, -- Enchant Ring - Zul'jin's Mastery
     },
-    [getCategoryId("enchants", "WEAPON")] = { -- done
+    [getCategoryId("enchants", "WEAPON")] = {
         { ["id"] = { 244028, 244029 } }, -- Enchant Weapon - Acuity of the Ren'dorei
         { ["id"] = { 244030, 244031 } }, -- Enchant Weapon - Arcane Mastery
         { ["id"] = { 243972, 243973 } }, -- Enchant Weapon - Berserker's Rage
@@ -91,32 +91,32 @@ Data.Items = {
     --     { ["id"] = { 223702, 223703, 223704 } }, -- Enchant Tool - Algari Perception
     --     { ["id"] = { 223705, 223706, 223707 } }, -- Enchant Tool - Algari Resourcefulness
     -- },
-    [getCategoryId("gems", "PRISMATIC_GEM")] = { -- done
+    [getCategoryId("gems", "PRISMATIC_GEM")] = {
         { ["id"] = { 240982, 240983 } }, -- Indecipherable Eversong Diamond
         { ["id"] = { 240966, 240967 } }, -- Powerful Eversong Diamond
         { ["id"] = { 240970, 240971 } }, -- Stoic Eversong Diamond
         { ["id"] = { 240968, 240969 } }, -- Telluric Eversong Diamond
 
     }
-    ,[getCategoryId("gems", "CRITICAL_STRIKE")] = { -- done
+    ,[getCategoryId("gems", "CRITICAL_STRIKE")] = {
         { ["Stat1"] = 1, ["Stat2"] = 1, ["id"] = { 240903, 240904 } }, -- Flawless Deadly Garnet
         { ["Stat1"] = 1, ["Stat2"] = 2, ["id"] = { 240905, 240906 } }, -- Flawless Quick Garnet
         { ["Stat1"] = 1, ["Stat2"] = 3, ["id"] = { 240907, 240908 } }, -- Flawless Masterful Garnet
         { ["Stat1"] = 1, ["Stat2"] = 4, ["id"] = { 240909, 240910 } }, -- Flawless Versatile Garnet
     },
-    [getCategoryId("gems", "HASTE")] = { -- done
+    [getCategoryId("gems", "HASTE")] = {
         { ["Stat1"] = 2, ["Stat2"] = 0, ["id"] = { 240887, 240888 } }, -- Flawless Quick Peridot
         { ["Stat1"] = 2, ["Stat2"] = 1, ["id"] = { 240889, 240890 } }, -- Flawless Deadly Peridot
         { ["Stat1"] = 2, ["Stat2"] = 3, ["id"] = { 240891, 240892 } }, -- Flawless Masterful Peridot
         { ["Stat1"] = 2, ["Stat2"] = 4, ["id"] = { 240893, 240894 } }, -- Flawless Versatile Peridot
     }
-    ,[getCategoryId("gems", "MASTERY")] = { -- done
+    ,[getCategoryId("gems", "MASTERY")] = {
         { ["Stat1"] = 3, ["Stat2"] = 1, ["id"] = { 240897, 240898 } }, -- Flawless Deadly Amethyst
         { ["Stat1"] = 3, ["Stat2"] = 2, ["id"] = { 240899, 240900 } }, -- Flawless Quick Amethyst
         { ["Stat1"] = 3, ["Stat2"] = 0, ["id"] = { 240895, 240896 } }, -- Flawless Masterful Amethyst
         { ["Stat1"] = 3, ["Stat2"] = 4, ["id"] = { 240901, 240902 } }, -- Flawless Versatile Amethyst
     }
-    ,[getCategoryId("gems", "VERSATILITY")] = { -- done
+    ,[getCategoryId("gems", "VERSATILITY")] = {
         { ["Stat1"] = 4, ["Stat2"] = 0, ["id"] = { 240911, 240912 } }, -- Flawless Versatile Lapis
         { ["Stat1"] = 4, ["Stat2"] = 1, ["id"] = { 240913, 240914 } }, -- Flawless Deadly Lapis
         { ["Stat1"] = 4, ["Stat2"] = 2, ["id"] = { 240915, 240916 } }, -- Flawless Quick Lapis
@@ -129,7 +129,7 @@ Data.Items = {
     --     { ["Stat1"] = 5, ["Stat2"] = 3, ["id"] = { 213507, 213508, 213509 } }, -- Masterful Amber
     --     { ["Stat1"] = 5, ["Stat2"] = 4, ["id"] = { 213510, 213511, 213512 } }, -- Versatile Amber
     -- }
-    ,[getCategoryId("consumables", "Runes")] = { -- done
+    ,[getCategoryId("consumables", "Runes")] = {
 
         { ["id"] = { 257749, 257750 } }, -- Laced Zoomshots
         { ["id"] = { 243735, 243736 } }, -- Oil of Dawn
@@ -141,7 +141,7 @@ Data.Items = {
         { ["id"] = { 257751, 257752 } }, -- Weighted Boomshots
 
     }
-    ,[getCategoryId("consumables", "Flasks")] = { -- done
+    ,[getCategoryId("consumables", "Flasks")] = {
         { ["id"] = { 241321, 241320 } }, -- Flask of Thalassian Resistance, 
         { ["id"] = { 241325, 241324 } }, -- Flask of the Blood Knights, 
         { ["id"] = { 241323, 241322 } }, -- Flask of the Magisters, 
@@ -152,7 +152,7 @@ Data.Items = {
         { ["id"] = { 241317, 241316 } }, -- Haranir Phial of Perception, 
         { ["id"] = {  241334 } }, -- Vicious Thalassian Flask of Honor, 241335,
     }
-    ,[getCategoryId("consumables", "Potions")] = { -- done
+    ,[getCategoryId("consumables", "Potions")] = {
         { ["id"] = {241299, 241298} }, -- Amani Extract
         { ["id"] = {241293, 241292} }, -- Draught of Rampant Abandon
         { ["id"] = {241339, 241338} }, -- Enlightenment Tonic
@@ -166,7 +166,7 @@ Data.Items = {
         { ["id"] = {241307, 241306} }, -- Refreshing Serum
         { ["id"] = {241305, 241304} }, -- Silvermoon Health Potion
     }
-    ,[getCategoryId("consumables", "Food")] = { -- done
+    ,[getCategoryId("consumables", "Food")] = {
         { ["id"] = 255846 }, -- Harandar Celebration
         { ["id"] = 255845 }, -- Silvermoon Parade
         { ["id"] = 242273 }, -- Blooming Feast
@@ -196,22 +196,22 @@ Data.Items = {
         { ["id"] = 242290 }, -- Wise Tails
         { ["id"] = 242296 }, -- Bloodthistle-wrapped Cutlets
     }
-    ,[getCategoryId("optionalReagents", "Algari Missive", "CRITICAL_STRIKE")] = { -- done
+    ,[getCategoryId("optionalReagents", "Algari Missive", "CRITICAL_STRIKE")] = {
         { ["Stat1"] = 1, ["Stat2"] = 2, ["id"] = { 245785, 245786 } }, -- Thalassian Missive of the Fireflash
         { ["Stat1"] = 1, ["Stat2"] = 3, ["id"] = { 245789, 245790 } }, -- Thalassian Missive of the Peerless
         { ["Stat1"] = 1, ["Stat2"] = 4, ["id"] = { 245791, 245792 } }, -- Thalassian Missive of the Quickblade
     }
-    ,[getCategoryId("optionalReagents", "Algari Missive", "HASTE")] = { -- done
+    ,[getCategoryId("optionalReagents", "Algari Missive", "HASTE")] = {
         { ["Stat1"] = 2, ["Stat2"] = 1, ["id"] = { 245785, 245786 } }, -- Thalassian Missive of the Fireflash
         { ["Stat1"] = 2, ["Stat2"] = 3, ["id"] = { 245784, 245783 } }, -- Thalassian Missive of the Feverflare
         { ["Stat1"] = 2, ["Stat2"] = 4, ["id"] = { 245781, 245782 } }, -- Thalassian Missive of the Aurora                   
     }
-    ,[getCategoryId("optionalReagents", "Algari Missive", "MASTERY")] = { -- done
+    ,[getCategoryId("optionalReagents", "Algari Missive", "MASTERY")] = {
         { ["Stat1"] = 3, ["Stat2"] = 1, ["id"] = { 245789, 245790 } }, -- Thalassian Missive of the Peerless
         { ["Stat1"] = 3, ["Stat2"] = 2, ["id"] = { 245784, 245783 } }, -- Thalassian Missive of the Feverflare
         { ["Stat1"] = 3, ["Stat2"] = 4, ["id"] = { 245787, 245788 } }, -- Thalassian Missive of the Harmonious
     }
-    ,[getCategoryId("optionalReagents", "Algari Missive", "VERSATILITY")] = { -- done
+    ,[getCategoryId("optionalReagents", "Algari Missive", "VERSATILITY")] = {
         { ["Stat1"] = 4, ["Stat2"] = 1, ["id"] = { 245791, 245792 } }, -- Thalassian Missive of the Quickblade
         { ["Stat1"] = 4, ["Stat2"] = 2, ["id"] = { 245781, 245782 } }, -- Thalassian Missive of the Aurora
         { ["Stat1"] = 4, ["Stat2"] = 3, ["id"] = { 245787, 245788 } }, -- Thalassian Missive of the Harmonious

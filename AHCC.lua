a7bfe4ad2b8eb110481302166993a506a1becbd4
muhmiauwau <muhmiauwau@users.noreply.github.com>
2026-03-04 14:33:51 +0100

    bug fix 3

--- AHCC.lua
+++ AHCC.lua
@@ -34,12 +34,7 @@ function AHCC:OnInitialize()
     AHCC.Config.ProfessionsQualityActive = self.db.char.QualitySelected
     AHCCItems:Init()
 
-print("loaded")
-
-
     self.PriceScan = CreateFrame("Frame", nil, AHCCBrowseResultsFrame, "AHCCPriceScanTemplate")
-
-
 
 	local checktime  = time() - (60 * 6)
 	if not self.db.global.lastScan or (self.db.global.lastScan and self.db.global.lastScan < checktime)  then
--- AHCCPriceScan.lua
+++ AHCCPriceScan.lua
@@ -46,7 +46,6 @@ function AHCCPriceScanMixin:startScan()
         
         self.inProgress = true
         self:RegisterForEvents()
-        print("repl")
         C_AuctionHouse.ReplicateItems()
     else
         self:SetText(L["cannot_initiate_scan"])

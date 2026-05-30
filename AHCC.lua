9d2c399354f4a7c7b8647eeeca1c4d96c761f953
Darksky1991 <hnlyfyj1991@gmail.com>
2026-05-30 00:42:29 +0000

    use warband locale

--- AHCC.lua
+++ AHCC.lua
@@ -19,7 +19,7 @@ local DBdefaults = {
         customCategories = {
             {
                 order = 1,
-                name = "My Warband Items",
+                name = "L["My Warband Items"]",
                 id = "myItems",
                 categories = {}
             }
